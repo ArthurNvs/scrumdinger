3f08955a56e23e285a1cc2b2b2a9292fc133029a
Arthur Neves <arthur.monteiro.ext@sodexo.com>
2021-10-22 08:06:16 -0300

    navigation bettween views, accessibility and views construction

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -10,7 +10,38 @@ import SwiftUI
 struct DetailView: View {
     let scrum: DailyScrum
     var body: some View {
-        Text("Daily Scrum details")
+        List {
+            Section(header: Text("Meeting info")) {
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                    .accessibilityLabel(Text("Start meeting"))
+                }
+                HStack {
+                    Label("Length", systemImage: "clock")
+                        .accessibilityLabel(Text("Meeting length"))
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                HStack {
+                    Label("Color", systemImage: "paintpalette")
+                    Spacer()
+                    Image(systemName: "checkmark.circle.fill")
+                        .foregroundColor(scrum.color)
+                }
+                .accessibilityElement(children: .ignore)
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees, id: \.self) { attendee in
+                    Label(attendee, systemImage: "person")
+                        .accessibilityLabel(Text("Attendee"))
+                        .accessibilityValue(Text(attendee))
+                }
+            }
+        }
+        .listStyle(InsetGroupedListStyle())
+        .navigationTitle(scrum.title)
     }
 }
 
--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -12,7 +12,7 @@ struct ScrumsView: View {
     var body: some View {
         List {
             ForEach(scrums) { scrum in
-                NavigationLink(destination: Text(scrum.title)) {
+                NavigationLink(destination: DetailView(scrum: scrum)) {
                     CardView(scrum: scrum)
                         .listRowBackground(scrum.color)
                 }
@@ -31,6 +31,6 @@ struct ScrumsView_Previews: PreviewProvider {
         NavigationView {
             ScrumsView(scrums: DailyScrum.data)
         }
-        .preferredColorScheme(.dark)
+        
     }
 }

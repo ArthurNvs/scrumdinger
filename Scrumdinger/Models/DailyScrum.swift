e6dbc51312158181169d0a307e48e6fb4db6c9f4
Arthur Neves <arthur.monteiro.ext@sodexo.com>
2021-10-23 09:31:58 -0300

    EditView for scrum design and first steps

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -22,7 +22,7 @@ struct DailyScrum: Identifiable {
         self.color = color
     }
 }
-    
+
 extension DailyScrum {
     static var data: [DailyScrum] {
         [
@@ -30,5 +30,18 @@ extension DailyScrum {
             DailyScrum(title: "App Dev", attendees: ["Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, color: Color("App Dev")),
             DailyScrum(title: "Web Dev", attendees: ["Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 1, color: Color("Web Dev"))
         ]
+    }
+}
+
+extension DailyScrum {
+    struct Data {
+        var title: String = ""
+        var attendees: [String] = []
+        var lengthInMinutes: Double = 5.0
+        var color: Color = .random
+    }
+    
+    var data: Data {
+        return Data(title: title, attendees: attendees, lengthInMinutes: Double(lengthInMinutes), color: color)
     }
 }
